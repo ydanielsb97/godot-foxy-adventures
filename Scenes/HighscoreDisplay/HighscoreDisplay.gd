c68689cb605e163e71179f66b2f93532257f62ef
Yeison Serrano <ydanielsb97@gmail.com>
2025-05-05 12:29:37 -0400

    hud with score saving working 100%

--- Scenes/HighscoreDisplay/HighscoreDisplay.gd
+++ Scenes/HighscoreDisplay/HighscoreDisplay.gd
@@ -1,10 +1,23 @@
 extends VBoxContainer
 
+class_name HighscoreDisplayItem
 
-# Called when the node enters the scene tree for the first time.
+@onready var score_label: Label = $ScoreLabel
+@onready var date_label: Label = $DateLabel
+
+var _highscore: HighScore
+
 func _ready() -> void:
 	start_fading()
+	set_label()
+
+func setup(highscore: HighScore) -> void:
+	_highscore = highscore
 
 func start_fading() -> void:
 	var tween = create_tween()
 	tween.tween_property(self, "modulate", Color("#ffffff", 1.0), 1)
+
+func set_label() -> void:
+	score_label.text = "%05d" % _highscore.score
+	date_label.text = _highscore.date_scored
--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -1,5 +1,20 @@
 extends Control
 
+@onready var highscores_container: GridContainer = $MarginContainer/HighscoresContainer
+
+const HIGHSCORE_DISPLAY = preload("res://Scenes/HighscoreDisplay/HighscoreDisplay.tscn")
+
+func _ready() -> void:
+	load_highscores()
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		GameManager.load_next_level()
+
+func load_highscores() -> void:
+	var highscores: Array[HighScore] = GameManager.high_scores.get_scores_list()
+	
+	for highscore in highscores:
+		var highscore_display = HIGHSCORE_DISPLAY.instantiate()
+		highscore_display.setup(highscore)
+		highscores_container.add_child(highscore_display)
